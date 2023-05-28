95c9aa59ffa8aa7cad6979d0ea9d97c9c8548fa7
Derek Sutton <dereksutton86@gmail.com>
2023-05-28 15:37:57 -0400

    added api routes for reactions

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -21,4 +21,14 @@ router
     .put(updateThought)
     .delete(deleteThought);
 
+// /api/thoughts/:thoughtId/reactions
+router
+    .route('/:thoughtId/reactions')
+    .post(addReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router
+    .route('/:thoughtId/reactions/:reactionId')
+    .delete(removeReaction);
+
 module.exports = router;
