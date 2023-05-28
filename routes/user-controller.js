f1abd59316c6ee69c46b8dfd3a0359d896003ea5
Derek Sutton <dereksutton86@gmail.com>
2023-05-28 13:52:03 -0400

    defined userController route functions

---
+++ routes/user-controller.js
@@ -0,0 +1,66 @@
+const { User } = require('../models');
+
+const userController = {
+
+    // Get all users
+    getAllUsers(req, res) {
+        User.find({})
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+        });
+    },
+
+    // Get one user by id
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+        .then(dbUserData => {
+            // If no user found, send 404
+            if (!dbUserData) {
+                res.sendStatus(404);
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+        });
+    },
+
+    // createUser
+    createUser({ body }, res) {
+        User.create(body)
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.json(err));
+    },
+
+    // updateUser
+    updateUser({  params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+            .then(dbuserData => {
+                if (!dbUserData) {
+                    res.sendStatus(404);
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
+    },
+
+    // deleteUser
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.sendStatus(404);
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
+    }
+};
+
+module.exports = userController;
