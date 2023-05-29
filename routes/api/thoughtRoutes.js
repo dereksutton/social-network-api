ba545e0acb814b590dc3ff233ee3539ec0938de7
Derek Sutton <dereksutton86@gmail.com>
2023-05-29 15:33:06 -0400

    added start script in package.json, added route imports for reactions to thought-controller

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -5,7 +5,9 @@ const {
     getThoughtById,
     createThought,
     updateThought,
-    deleteThought
+    deleteThought,
+    addReaction,
+    removeReaction
 } = require('../../controllers/thought-controller');
 
 // /api/thoughts
