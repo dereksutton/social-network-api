ba865339bf042f947275069aaa6d2f2cd2fb583b
Derek Sutton <dereksutton86@gmail.com>
2023-05-31 22:32:07 -0400

    added controllers for adding/removing friends from users friend list

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -61,6 +61,34 @@ const userController = {
             })
             .catch(err => res.json(err));
     }
+
+    // addFriend
+    addFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $push: { friends: params.friendId } },
+            { new: true }
+        )
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id!' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.json(err));
+    },
+
+    // removeFriend
+    removeFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $pull: { friends: params.friendId } },
+            { new: true }
+        )
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.json(err));
+    }
 };
 
 module.exports = userController;
